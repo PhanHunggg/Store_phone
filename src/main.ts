e43ed1b3be7d97194e0210e262712da5a5bc31f7
PhanHung <phanquochung280702@gmail.com>
2023-12-05 08:18:37 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder().setTitle("Swagger").addBearerAuth().build()
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("swagger", app, document);
+
+
+  app.enableCors()
+  app.use(express.static("."))
+  await app.listen(8080);
+}
+bootstrap();
